6ec83e81967b14b8a6215ceea8fa59afa223fcc8
justinkiang <justin@justinkiang.com>
2024-01-18 15:25:41 -0800

    Renaming

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "beth-ios",
+    name: "Beth",
     products: [
         .library(
-            name: "beth-ios",
-            targets: ["beth-ios"]),
+            name: "beth",
+            targets: ["Beth"]),
     ],
     targets: [
         .binaryTarget(
-            name: "beth-ios",
+            name: "Beth",
             url: "https://github.com/opusx-ai/beth-ios/releases/download/v1.0.0/beth-ios.xcframework.zip",
             checksum: "99b1a96d0ca7d0b3db6b9b18d79f43872cc9a0632936b8f59f5233c501017489")
     ]
