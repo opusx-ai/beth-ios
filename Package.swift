2db282f97ce837f043bbe13f8400b12597851313
justinkiang <justin@justinkiang.com>
2024-01-18 16:12:53 -0800

    Moved files

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Beth",
-            url: "https://github.com/opusx-ai/beth-ios/releases/download/v1.0.2/beth.xcframework.zip",
+            url: "https://github.com/opusx-ai/beth-ios/releases/download/v1.0.3/beth.xcframework.zip",
             checksum: "fe27004c8516ae29d0c584fa15e6ad7cf7ab77162b6ead79e232d5c45b0820cd")
     ]
 )
